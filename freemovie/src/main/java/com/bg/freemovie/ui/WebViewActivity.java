1fdf92fa3473e6cbcfd1c9ba3b5f8362701409ae
wardenlzr <wardenlzr@qq.com>
2018-07-03 14:13:22 +0800

    添加领红包支持作者

--- freemovie/src/main/java/com/bg/freemovie/ui/WebViewActivity.java
+++ freemovie/src/main/java/com/bg/freemovie/ui/WebViewActivity.java
@@ -1,7 +1,10 @@
 package com.bg.freemovie.ui;
 
 import android.app.Activity;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -43,11 +46,11 @@ public class WebViewActivity extends BaseActivity {
     @Override
     public void initView(Bundle savedInstanceState, View contentView) {
         String url = getIntent().getStringExtra("url");
-        AgentWeb mAgentWeb  = AgentWeb.with(this)
+        AgentWeb mAgentWeb = AgentWeb.with(this)
                 .setAgentWebParent((LinearLayout) findViewById(R.id.ll_web), new LinearLayout.LayoutParams(-1, -1))
                 .useDefaultIndicator()
                 .createAgentWeb()
-//                .ready()
+                .ready()
                 .go(url);
         webView = mAgentWeb.getWebCreator().getWebView();
         String userAgent = webView.getSettings().getUserAgentString();
@@ -57,30 +60,38 @@ public class WebViewActivity extends BaseActivity {
                     .replace("android", "")
                     + " cldc");
         }
-        LogUtils.e("initView.url:"+webView.getUrl());
+        LogUtils.e("initView.url:" + webView.getUrl());
         findViewById(R.id.fab).setOnClickListener(view -> {
             String webViewUrl = webView.getUrl();
             if (webViewUrl.contains("www.iqiyi") || webViewUrl.contains("v.qq") || webViewUrl.contains("v.youku") || webViewUrl.contains("new-play.tudou")) {
                 MoviePlayerActivity.start(mContext, webViewUrl);
-            }else {
+            } else if (webViewUrl.contains("wardenlzr.github")) {
+                ToastUtils.showShort("领个红包支持下作者");
+                ClipboardManager cm = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                // 将文本内容放到系统剪贴板里。
+                assert cm != null;
+                cm.setText("RNtf3D86jX");
+                try {
+                    PackageManager packageManager
+                            = this.getApplicationContext().getPackageManager();
+                    Intent intent = packageManager.
+                            getLaunchIntentForPackage("com.eg.android.AlipayGphone");
+                    startActivity(intent);
+                }catch (Exception e) {
+                    e.printStackTrace();
+                    ToastUtils.showShort("尴尬死...");
+                }
+            } else {
                 ToastUtils.showLong("要看哪个你先点进去啊！");
             }
         });
     }
-    public static void start(Activity context, String url){
+
+    public static void start(Activity context, String url) {
         Intent intent = new Intent(context, WebViewActivity.class);
         intent.putExtra("url", url);
-        LogUtils.e("start.url:"+url);
+        LogUtils.e("start.url:" + url);
         context.startActivity(intent);
         context.overridePendingTransition(com.lzr.warden.terrificlibrary.R.anim.slide_in_left, com.lzr.warden.terrificlibrary.R.anim.slide_out_left);
-    }
-
-    @Override
-    public void onBackPressed() {
-        LogUtils.e("onBackPressed.url:"+webView.getUrl());
-        if (webView.canGoBack()) {
-
-        }
-        super.onBackPressed();
     }
 }
